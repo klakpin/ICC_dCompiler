6cf54cf1c90b33af93347eb797399417e5c5e5f0
ilya <klakpin@gmail.com>
2017-11-16 14:55:42 +0300

    Updates

--- src/runtime/implementations/OperationsImpl.java
+++ src/runtime/implementations/OperationsImpl.java
@@ -2,6 +2,8 @@ package implementations;
 
 import Interfaces.Operations;
 
+import java.util.Objects;
+
 //TODO implement everything
 public class OperationsImpl implements Operations {
 
@@ -52,94 +54,92 @@ public class OperationsImpl implements Operations {
     @Override
     public Boolean greater(Object var1, Object var2) {
         if (var1 instanceof Integer && var2 instanceof Integer) {
-            Integer a = (Integer) var1;
-            Integer b = (Integer) var2;
-            return a > b;
+            return (Integer) var1 > (Integer) var2;
         }
-        //TODO write all possible operations
+        if (var1 instanceof Integer && var2 instanceof Double) {
+            return (Integer) var1 > (Double) var2;
+        }
+        if (var1 instanceof Double && var2 instanceof Integer) {
+            return (Double) var1 > (Integer) var2;
+        }
+        if (var1 instanceof Double && var2 instanceof Double) {
+            return (Double) var1 > (Double) var2;
+        }
+
         return null;
     }
 
     @Override
     public Boolean less(Object var1, Object var2) {
         if (var1 instanceof Integer && var2 instanceof Integer) {
-            Integer a = (Integer) var1;
-            Integer b = (Integer) var2;
-            return a < b;
+            return (Integer) var1 < (Integer) var2;
         }
-        //TODO write all operations
+        if (var1 instanceof Integer && var2 instanceof Double) {
+            return (Integer) var1 < (Double) var2;
+        }
+        if (var1 instanceof Double && var2 instanceof Integer) {
+            return (Double) var1 < (Integer) var2;
+        }
+        if (var1 instanceof Double && var2 instanceof Double) {
+            return (Double) var1 < (Double) var2;
+        }
         return null;
     }
 
     @Override
     public Boolean notequal(Object var1, Object var2) {
+
         if (var1 instanceof Integer && var2 instanceof Integer) {
-            Integer a = (Integer) var1;
-            Integer b = (Integer) var2;
+            return var1 != var2;
+        }
+        if (var1 instanceof Integer && var2 instanceof Double) {
+            int a = (Integer) var1;
+            double b = (Double) var2;
+            return a != b;
+
+        }
+        if (var1 instanceof Double && var2 instanceof Integer) {
+            double a = (Double) var1;
+            int b = (Integer) var2;
             return a != b;
         }
-        //TODO write all possible operations
+        if (var1 instanceof Double && var2 instanceof Double) {
+            return var1 != var2;
+        }
         return null;
     }
 
     @Override
-    public Boolean moreequals(Object var1, Object var2) {
-        return null;
-    }
-
-    @Override
-    public Boolean lessequals(Object var1, Object var2) {
+    public Boolean greaterequals(Object var1, Object var2) {
         if (var1 instanceof Integer && var2 instanceof Integer) {
-            Integer a = (Integer) var1;
-            Integer b = (Integer) var2;
-            return a <= b;
+            return (Integer) var1 >= (Integer) var2;
         }
-
+        if (var1 instanceof Integer && var2 instanceof Double) {
+            return (Integer) var1 >= (Double) var2;
+        }
+        if (var1 instanceof Double && var2 instanceof Integer) {
+            return (Double) var1 >= (Integer) var2;
+        }
+        if (var1 instanceof Double && var2 instanceof Double) {
+            return (Double) var1 >= (Double) var2;
+        }
         return null;
     }
 
     @Override
-    public Boolean and(Object var1, Object var2) {
-        if (var1 instanceof Boolean && var2 instanceof Boolean) {
-            Boolean a = (Boolean) var1;
-            Boolean b = (Boolean) var2;
-            return a && b;
+    public Boolean lessequal(Object var1, Object var2) {
+        if (var1 instanceof Integer && var2 instanceof Integer) {
+            return (Integer) var1 <= (Integer) var2;
         }
-
-        return null;
-    }
-
-    @Override
-    public Boolean or(Object var1, Object var2) {
-        if (var1 instanceof Boolean && var2 instanceof Boolean) {
-            Boolean a = (Boolean) var1;
-            Boolean b = (Boolean) var2;
-            return a || b;
+        if (var1 instanceof Integer && var2 instanceof Double) {
+            return (Integer) var1 <= (Double) var2;
         }
-
-        return null;
-    }
-
-    @Override
-    public Boolean xor(Object var1, Object var2) {
-
-        if (var1 instanceof Boolean && var2 instanceof Boolean) {
-            Boolean a = (Boolean) var1;
-            Boolean b = (Boolean) var2;
-            return a ^ b;
+        if (var1 instanceof Double && var2 instanceof Integer) {
+            return (Double) var1 <= (Integer) var2;
         }
-
-        return null;
-    }
-
-    @Override
-    public Boolean not(Object var1, Object var2) {
-        if (var1 instanceof Boolean) {
-            Boolean a = (Boolean) var1;
-
-            return !a;
+        if (var1 instanceof Double && var2 instanceof Double) {
+            return (Double) var1 <= (Double) var2;
         }
-
         return null;
     }
 
@@ -156,6 +156,69 @@ public class OperationsImpl implements Operations {
 
     @Override
     public Boolean equals(Object var1, Object var2) {
+        if (var1 instanceof Integer && var2 instanceof Integer) {
+            return (Integer) var1 == (Integer) var2;
+        }
+        if (var1 instanceof Integer && var2 instanceof Double) {
+            int a = (Integer) var1;
+            double b = (Double) var2;
+            return a == b;
+
+        }
+        if (var1 instanceof Double && var2 instanceof Integer) {
+            double a = (Double) var1;
+            int b = (Integer) var2;
+            return a == b;
+
+        }
+        if (var1 instanceof Double && var2 instanceof Double) {
+            return var1 == var2;
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean and(Object var1, Object var2) {
+        if (var1 instanceof Boolean && var2 instanceof Boolean) {
+            if (var1.equals(true) && var2.equals(true)) {
+                return true;
+            }
+            return false;
+        }
+        return null;
+
+    }
+
+    @Override
+    public Boolean or(Object var1, Object var2) {
+        if (var1 instanceof Boolean && var2 instanceof Boolean) {
+            if (var1.equals(false) && var2.equals(false)) {
+                return false;
+            }
+            return true;
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean xor(Object var1, Object var2) {
+        if (var1 instanceof Boolean && var2 instanceof Boolean) {
+            if ((var1.equals(true) && var2.equals(true)) || (var1.equals(false) && var2.equals(false))) {
+                return false;
+            }
+            return true;
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean not(Object var1) {
+        if (var1 instanceof Boolean) {
+            if (var1.equals(true)) {
+                return false;
+            }
+            return true;
+        }
         return null;
     }
 
