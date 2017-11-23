9a9fa44849055fd4ac328e65f023a4b46276db49
ALYONA\Alyona <zaa145@yandex.ru>
2017-11-24 00:38:05 +0300

    added error message

--- src/runtime/main/Main.java
+++ src/runtime/main/Main.java
@@ -3,6 +3,10 @@ package main;
 
 import implementations.Output;
 
+import java.io.File;
+import java.util.Scanner;
+
+
 /**
  * Entry point in compiled program
  */
@@ -15,6 +19,20 @@ public class Main {
         try {
             run.run();
         } catch (Exception e) {
+            Scanner scanner = new Scanner(new File("mapping"));
+            String str=scanner.nextLine();
+            Integer lineNumber=e.getStackTrace()[1].getLineNumber();
+            boolean flag=false;
+            while(scanner.hasNext()&&!flag){
+               if (str.contains(lineNumber.toString())){
+                   System.out.println(lineNumber.toString().length());
+                   System.out.println("Attention! Error in "+str.substring(lineNumber.toString().length()+1)+" line.");
+                   flag=true;
+               }
+                str=scanner.nextLine();
+//                System.out.println("STR="+str);
+            }
+            System.out.println("Result"+str);
             e.printStackTrace();
             System.out.println(e.getStackTrace()[1].getLineNumber());
             System.out.println(e.getMessage());
